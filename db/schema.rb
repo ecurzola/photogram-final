e889fc648479b0f6a9a0b797dd284217f605e80a
ecurzola <146590441+ecurzola@users.noreply.github.com>
2023-12-05 04:00:00 +0000

    photo model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_05_035805) do
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "comments_count"
+    t.string "image"
+    t.integer "likes_count"
+    t.integer "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
